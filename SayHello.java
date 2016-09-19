5e684f488d5f4f47dabcd0ace446f273739ed3a6
WangZhiyuan <wang_zhiyuan1@hoperun.com>
2016-09-19 17:52:30 +0800

    add SayHello.java

--- SayHello.java
+++ SayHello.java
@@ -1,12 +1,6 @@
 public class SayHello {
-	private static void eat() {
-	
-	}
-
 	public static void main(String[] args) {
 		System.out.println("Hello");
 	}
 
-	private static void sleep() {
-	}
 }
